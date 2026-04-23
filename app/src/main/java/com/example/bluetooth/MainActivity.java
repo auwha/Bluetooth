8b530c0247559c6b8511222da6e5387256dc7ff5
Your Name <you@example.com>
2026-04-23 16:51:01 +0200

    Dodanie layoutu

--- app/src/main/java/com/example/bluetooth/MainActivity.java
+++ app/src/main/java/com/example/bluetooth/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.bluetooth;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +24,18 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ListView listViewPairedDevices = findViewById(R.id.listPairedDevices);
+
+        ArrayList<String> listPD = new ArrayList<>();
+        ArrayAdapter<String> arrayAdapterPD = new ArrayAdapter<String>(this, R.layout.list_view_items, R.id.row, listPD);
+        listViewPairedDevices.setAdapter(arrayAdapterPD);
+
+
+        ListView listViewNewDevices = findViewById(R.id.listNewDevices);
+
+        ArrayList<String> listND = new ArrayList<>();
+        ArrayAdapter<String> arrayAdapterND = new ArrayAdapter<String>(this, R.layout.list_view_items, R.id.row, listND);
+        listViewNewDevices.setAdapter(arrayAdapterPD);
     }
 }
